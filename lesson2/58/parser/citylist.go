67ca3c71253701d2fb90f53c0d3b9b6f0c27eb9d
coordinate <luliyucoordinate@outlook.com>
2019-06-27 18:52:07 +0800

    fix lesson2 citylist.go

--- lesson2/58/parser/citylist.go
+++ lesson2/58/parser/citylist.go
@@ -10,58 +10,47 @@ func ParseCityList(contents []byte) engine.ParseResult {
 	provinceList := make([]string, 0)
 
 	re := regexp.MustCompile(`provinceList = {([^}]*)`)
-	matches := re.FindAllSubmatch(contents, -1)
-	for _, m := range matches {
-		for _, subMatch := range m[1:] {
-			str := strings.Replace(string(subMatch), " ", "", -1)
-			str = strings.Replace(str, "\n", "", -1)
-			for _, sub := range strings.FieldsFunc(str, splitByComma) {
-				provinceList = append(provinceList, strings.Trim(strings.FieldsFunc(sub, splitBySemi)[0], `"`))
-			}
-		}
+	match := re.FindSubmatch(contents)
+	str := strings.Replace(string(match[1]), " ", "", -1)
+	str = strings.Replace(str, "\n", "", -1)
+	for _, sub := range strings.FieldsFunc(str, splitByComma) {
+		provinceList = append(provinceList, strings.Trim(strings.FieldsFunc(sub, splitBySemi)[0], `"`))
 	}
+
 
 	result := engine.ParseResult{}
 
 	re = regexp.MustCompile(`independentCityList = {([^}]*)`)
-	matches = re.FindAllSubmatch(contents, -1)
-	for _, m := range matches {
-		for _, subMatch := range m[1:] {
-			str := strings.Replace(string(subMatch), " ", "", -1)
-			str = strings.Replace(str, "\n", "", -1)
-			for _, sub := range strings.FieldsFunc(str, splitByComma) {
-				independentCityStr := strings.FieldsFunc(sub, splitBySemi)
-				independentCity := strings.Trim(independentCityStr[0], `"`)
-				independentCityAb := strings.FieldsFunc(strings.Trim(independentCityStr[1], `"`), splitByVertical)[0]
-				independentCityAbUrl := "https://" + independentCityAb + ".58.com"
-				result.Items = append(result.Items, independentCity)
-				result.Requests = append(result.Requests, engine.Request{
-					Url:independentCityAbUrl,
-					ParserFunc: engine.NilParser,
-				})
-			}
-		}
+	match = re.FindSubmatch(contents)
+	str = strings.Replace(string(match[1]), " ", "", -1)
+	str = strings.Replace(str, "\n", "", -1)
+	for _, sub := range strings.FieldsFunc(str, splitByComma) {
+		independentCityStr := strings.FieldsFunc(sub, splitBySemi)
+		independentCity := strings.Trim(independentCityStr[0], `"`)
+		independentCityAb := strings.FieldsFunc(strings.Trim(independentCityStr[1], `"`), splitByVertical)[0]
+		independentCityAbUrl := "https://" + independentCityAb + ".58.com"
+		result.Items = append(result.Items, independentCity)
+		result.Requests = append(result.Requests, engine.Request{
+			Url:independentCityAbUrl,
+			ParserFunc: engine.NilParser,
+		})
 	}
 
 	for _, province := range provinceList {
-		re = regexp.MustCompile(province + `":{([^}]*)`)
-		matches = re.FindAllSubmatch(contents, -1)
-		for _, m := range matches {
-			for _, subMatch := range m[1:] {
-				str := strings.Replace(string(subMatch), " ", "", -1)
-				str = strings.Replace(str, "\n", "", -1)
-				for _, sub := range strings.FieldsFunc(str, splitByComma) {
-					cityStr := strings.FieldsFunc(sub, splitBySemi)
-					city := strings.Trim(cityStr[0], `"`)
-					cityAb := strings.FieldsFunc(strings.Trim(cityStr[1], `"`), splitByVertical)[0]
-					cityAbUrl := "https://" + cityAb + ".58.com"
-					result.Items = append(result.Items, city)
-					result.Requests = append(result.Requests, engine.Request{
-						Url:cityAbUrl,
-						ParserFunc: engine.NilParser,
-					})
-				}
-			}
+		re := regexp.MustCompile(province + `":{([^}]*)`)
+		match = re.FindSubmatch(contents)
+		str = strings.Replace(string(match[1]), " ", "", -1)
+		str = strings.Replace(str, "\n", "", -1)
+		for _, sub := range strings.FieldsFunc(str, splitByComma) {
+			cityStr := strings.FieldsFunc(sub, splitBySemi)
+			city := strings.Trim(cityStr[0], `"`)
+			cityAb := strings.FieldsFunc(strings.Trim(cityStr[1], `"`), splitByVertical)[0]
+			cityAbUrl := "https://" + cityAb + ".58.com"
+			result.Items = append(result.Items, city)
+			result.Requests = append(result.Requests, engine.Request{
+				Url:cityAbUrl,
+				ParserFunc: engine.NilParser,
+			})
 		}
 	}
 	return result
